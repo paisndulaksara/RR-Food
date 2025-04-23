bc71b3671b4ad7f67600f772d5463caa671cb936
Paindu <pasindulaksara@gmail.com>
2025-04-23 13:10:38 +0530

    changes in slug page

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,6 +1,6 @@
 // app/products/[slug]/page.tsx
 import ProductDetail from '@/components/products/ProductDetail';
-
+import { getAllProducts } from '@/lib/products';
 export type ParamsType = Promise<{ slug: string }>;
 
 interface PageProps {
@@ -11,3 +11,10 @@ export default async function ProductDetailPage({ params }: PageProps) {
   const { slug } = await params;
   return <ProductDetail productSlug={slug} />;
 }  
+
+export async function generateStaticParams() {
+  const products = getAllProducts();
+  return products.map((product) => ({
+    slug: product.slug,
+  }));
+}
